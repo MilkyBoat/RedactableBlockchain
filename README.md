5db918c57f7cfd024775a7c0ca884eadcbe955d0
MilkyBoat <xuyunkai@mail.nankai.edu.cn>
2021-04-05 19:36:49 +0800

    dev 0

--- README.md
+++ README.md
@@ -9,5 +9,7 @@ ganache-cli -l 9007199254740991 --db ./RDBlockchain -s 1234
 ## Complie contracts and deploy them
 
 ```bash
-
+truffle conplie
+cd app
+python3 RDChain.py
 ```
--- contracts/RDChain.sol
+++ contracts/RDChain.sol
@@ -1,24 +1,43 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.16 <0.9.0;
+pragma solidity >=0.5.16;
+
+import "./LibChameleonHash.sol";
 
 contract RDChain {
 
-    mapping (address => uint) private saltList;
+    using LibChameleonHash for *;
+
+    uint private maxNo;
+    mapping (uint => address) private hashList;
+    mapping (uint => string) private msgList;
+    mapping (uint => uint) private rList;
 
     function helloworld() public pure returns(string memory) {
         return "hello world!";
     }
 
-    function extendChain(string memory m) public returns (bool) {
+    function chainInit() public {
+        maxNo = 0;
+        msgList[0] = "redactable blockchain genesis block";
+        hashList[0] = 0x0000000000000000000000000000000000000000;
+        rList[0] = 0;
+    }
+
+    function extendChain(string memory m) public pure returns (bool) {
+        
         return true;
     }
 
-    function redactBlock(address blockAddr, string memory m) public returns (bool) {
+    function redactBlock(uint blockNo, string memory m) public pure returns (bool) {
         return true;
     }
 
-    function delBlock(address blockAddr) public returns (bool) {
-        return true;
+    function delBlock(uint blockNo) public pure returns (bool) {
+        return redactBlock(blockNo, "");
+    }
+
+    function getBlock(uint blockNo) public view returns (string memory) {
+        return msgList[blockNo];
     }
 
 //   function insertBlock(uint blockNo, string memory m) public returns (bool) {
